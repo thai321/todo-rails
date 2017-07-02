06fdf04e55e22103f1bd3a233d5cb1e51889bbad
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-07-01 18:17:05 -0700

    todo rails

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,44 @@
+class TodosController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if (@todo.save)
+      flash[:notice] = "Todo was created successfully!"
+      redirect_to todo_path(@todo) # to show page
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if(@todo.update(todo_params))
+      flash[:notice] = "Todo was updated successfully"
+      redirect_to todos_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:name, :description)
+  end
+
+end
